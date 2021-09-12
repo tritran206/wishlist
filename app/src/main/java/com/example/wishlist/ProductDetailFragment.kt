a0d9e6ca7803501f2aa1c5546af04de49692bd7a
Tri Tran <tritran20@gmail.com>
2021-09-11 21:41:25 -0700

    Implemented the details fragment

--- app/src/main/java/com/example/wishlist/ProductDetailFragment.kt
+++ app/src/main/java/com/example/wishlist/ProductDetailFragment.kt
@@ -5,8 +5,12 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.activityViewModels
+import androidx.navigation.fragment.findNavController
+import com.bumptech.glide.Glide
 import com.example.wishlist.data.DataSource
+import com.example.wishlist.data.model.Product
 import com.example.wishlist.databinding.FragmentProductDetailBinding
 
 private const val ARG_PRODUCT_INDEX = "productIndex"
@@ -17,6 +21,8 @@ class ProductDetailFragment : Fragment() {
     private val binding get() = _binding!!
     private val viewModel:ProductViewModel by activityViewModels()
     private var productIndex: Int? = null
+    private var product: Product? = null
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -31,15 +37,32 @@ class ProductDetailFragment : Fragment() {
     ): View? {
         _binding = FragmentProductDetailBinding.inflate(inflater, container, false)
         val productList = viewModel.productList
-        val product = productList?.get(productIndex!!)
+        product = productList?.get(productIndex!!)
+        bindButton()
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
         binding.textViewProductName.text = product?.name
         binding.textViewProductPrice.text = product?.getPrice()
-        return binding.root
+        binding.textViewDescription.text = product?.description
+        Glide.with(binding.root)
+            .load(product?.pictureUrl)
+            .into(binding.imageViewProduct)
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    private fun bindButton() {
+        binding.buttonBuy.setOnClickListener {
+            Toast.makeText(activity, "Thanks for buying ${product?.name}", Toast.LENGTH_LONG).show()
+            this.findNavController().popBackStack()
+
+        }
     }
 
 }
