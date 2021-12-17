9da8c6a34a26dc9faf670f6b0c17e65504f7a88e
Tony Tran <Tonyyytran@gmail.com>
2021-12-16 17:45:12 -0800

    fixed viewmodel factory to return the same instance of viewmodel

--- app/src/main/java/com/example/wishlist/ProductViewModelFactory.kt
+++ app/src/main/java/com/example/wishlist/ProductViewModelFactory.kt
@@ -7,11 +7,23 @@ import androidx.lifecycle.ViewModelProvider
 class ProductViewModelFactory (
     private val application: Application
 ) : ViewModelProvider.Factory {
+
     @Suppress("unchecked_cast")
     override fun <T : ViewModel?> create(modelClass: Class<T>): T {
         if (modelClass.isAssignableFrom(ProductViewModel::class.java)) {
-            return ProductViewModel(application) as T
+            return getInstance(application) as T
         }
         throw IllegalArgumentException("Unknown ViewModel class")
+    }
+
+    companion object {
+        private var instance: ProductViewModel? = null
+
+        fun getInstance(application: Application): ProductViewModel {
+            if (instance == null) {
+                instance = ProductViewModel(application)
+            }
+            return instance!!
+        }
     }
 }
